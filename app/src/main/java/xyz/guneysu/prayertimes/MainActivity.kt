34a597d67f8c1a0a27845b7144ce65e41f3088d0
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-01-04 21:22:42 +0300

    create meaningful and persistent notification

--- app/src/main/java/xyz/guneysu/prayertimes/MainActivity.kt
+++ app/src/main/java/xyz/guneysu/prayertimes/MainActivity.kt
@@ -13,28 +13,29 @@ import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.moshi.Moshi
 import io.swagger.client.models.PrayerTime
-import io.swagger.client.models.WeeklyPrayerTimes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        var channelId = createNotificationChannel();
-
-        simpleRequest()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        simpleRequest()
+    }
+
+    private fun createNotification(title: String, content: String) {
+        var channelId = createNotificationChannel();
 
         // Create an explicit intent for an Activity in your app
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
+
         val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
         val snoozeIntent = Intent(this, MainActivity::class.java).apply {
@@ -47,17 +48,24 @@ class MainActivity : AppCompatActivity() {
 
         var builder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("<< TITLE >>")
-            .setContentText(" << CONTENT >> ")
-            .setStyle(NotificationCompat.BigTextStyle()
-                .bigText("Much longer text that cannot fit one line..."))
+            .setContentTitle(title)
+            .setContentText(content)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Much longer text that cannot fit one line...")
+                    .setSummaryText("Summary Text")
+                    .setBigContentTitle("Content Title")
+            )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             // Set the intent that will fire when the user taps the notification
             .setContentIntent(pendingIntent)
-            .addAction(R.drawable.ic_launcher_foreground, getString(R.string.snooze),
-                snoozePendingIntent)
+            .addAction(
+                R.drawable.ic_launcher_foreground, getString(R.string.snooze),
+                snoozePendingIntent
+            )
             .setCategory(NotificationCompat.CATEGORY_REMINDER)
-            .setAutoCancel(true)
+            .setAutoCancel(false)
+            .setOngoing(true)
 
         with(NotificationManagerCompat.from(this)) {
             // notificationId is a unique int for each notification that you must define
@@ -93,14 +101,17 @@ class MainActivity : AppCompatActivity() {
     private fun simpleRequest() {
 // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val url = "https://virtserver.swaggerhub.com/guneysus/prayer-times/v1/istanbul/weekly"
+        val url = "https://virtserver.swaggerhub.com/guneysus/prayer-times/v1/istanbul/daily"
 
 // Request a string response from the provided URL.
         val stringRequest = StringRequest (
             Request.Method.GET, url,
             Response.Listener<String> { response ->
                 // Display the first 500 characters of the response string.
-                var prayerTimes = Moshi.Builder().build().adapter(WeeklyPrayerTimes::class.java).fromJson(response)
+                var p = Moshi.Builder().build().adapter(PrayerTime::class.java).fromJson(response)
+                var title = "${p?.fajr} | ${p?.sunrise} | ${p?.dhuhr} | ${p?.asr} | ${p?.maghrib} | ${p?.isha}";
+                createNotification(title = title, content = title)
+ 
                 Log.i("REQUEST_SUCCESS", "${response}")
             },
             Response.ErrorListener { err -> {
