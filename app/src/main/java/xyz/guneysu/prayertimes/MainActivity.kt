f81fa2d507d96c1dcf20f0430fc6029cd66dc4fd
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-01-04 11:34:18 +0300

    start activity on tap to the notification

--- app/src/main/java/xyz/guneysu/prayertimes/MainActivity.kt
+++ app/src/main/java/xyz/guneysu/prayertimes/MainActivity.kt
@@ -2,7 +2,9 @@ package xyz.guneysu.prayertimes
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+
         var builder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentTitle("<< TITLE >>")
@@ -24,6 +34,9 @@ class MainActivity : AppCompatActivity() {
             .setStyle(NotificationCompat.BigTextStyle()
                 .bigText("Much longer text that cannot fit one line..."))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)        
 
         with(NotificationManagerCompat.from(this)) {
             // notificationId is a unique int for each notification that you must define
