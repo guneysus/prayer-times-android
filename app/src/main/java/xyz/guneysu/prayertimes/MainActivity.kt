48e270ae8fb4254cc8e7da4bfeaaa6e49be78e02
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-01-04 11:29:21 +0300

    simple notification on startup

--- app/src/main/java/xyz/guneysu/prayertimes/MainActivity.kt
+++ app/src/main/java/xyz/guneysu/prayertimes/MainActivity.kt
@@ -1,12 +1,58 @@
 package xyz.guneysu.prayertimes
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var channelId = createNotificationChannel();
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("<< TITLE >>")
+            .setContentText(" << CONTENT >> ")
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText("Much longer text that cannot fit one line..."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            val notificationId = 0
+            notify(notificationId, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() : String {
+        var channelId : String = "";
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            channelId = getString(R.string.CHANNEL_ID);
+
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+        return channelId;
     }
 }
